0f6dd5a2db19a9e04b584810e46904f6ac8ff91a
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-11-08 03:30:50 +0200

    pnpm nx g remove ku-ui

--- apps/ku-ui/src/app/state/handlebar/handlebar.reducer.ts
+++
@@ -1,8 +0,0 @@
-import { EnvColor, HandlebarState } from '@greedy-coin/types';
-import { createReducer } from '@ngrx/store';
-
-const initialState: HandlebarState = {
-  envColor: EnvColor.RED
-};
-
-export const handlebarReducer = createReducer(initialState);

0f4b8dce49ac3e6d2da51f300c96e5aeda0b2fd4
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-11-08 04:34:56 +0200

    commit

--- apps/ku-ui/src/app/state/root.reducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import { handlebarReducer } from './handlebar/handlebar.reducer';
-
-export const rootReducer = combineReducers({
-  handlebar: handlebarReducer,
-});

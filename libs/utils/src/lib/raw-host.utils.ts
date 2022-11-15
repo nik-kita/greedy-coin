b99db8028781925233e926d16456a5baade912af
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-11-15 22:13:05 +0200

    rm utils

--- libs/utils/src/lib/raw-host.utils.ts
+++
@@ -1,7 +0,0 @@
-export function rawHost(host: `${'http' | 'https'}${string}`) {
-  const splited = host.split(/https?:\/\//);
-
-  return splited.length === 2
-    ? splited[1]
-    : splited[0];
-}

b76d9edba5c68e1fedd88ee2ae59d727aa020afe
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-11-15 22:12:12 +0200

    rm ku-ui

--- apps/ku-ui/src/app/state/hooks.state.ts
+++
@@ -1,5 +0,0 @@
-import { UiDispatch, UiState } from '@greedy-coin/types';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-
-export const useAppDispatch: () => UiDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<UiState> = useSelector;
--- apps/ku-ui/src/app/state/root.store.ts
+++
@@ -1,9 +0,0 @@
-import { UiState } from '@greedy-coin/types';
-import { configureStore } from '@reduxjs/toolkit';
-import { handlebarReducer } from './handlebar/handlebar.slice';
-
-export const store = configureStore<UiState>({
-  reducer: {
-    handlebar: handlebarReducer
-  },
-});

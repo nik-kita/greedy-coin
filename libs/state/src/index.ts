40e50a4574fe54b4b1bb04558f5432f5e3973d82
Nik Moiseienko <nik.moiseienko@gmail.com>
2022-11-29 02:37:28 +0200

    state <=> types/state

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -1,2 +1,8 @@
-export const none = null;
+import { RootStateType } from '@greedy-coin/types/state';
+import { configureStore } from '@reduxjs/toolkit';
 
+export const store = configureStore<RootStateType>({
+  reducer: {},
+});
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
